d5976306c874b2003f3c779f2bc5e5a8dd3d71d6
Redani Omar <101639366+RedaniOmar@users.noreply.github.com>
2023-01-11 20:07:00 +0100

    implementing navigation component

---
+++ app/src/main/java/com/example/rides/feature/presentation/ui/vehiclelist/VehicleListFragment.kt
@@ -0,0 +1,17 @@
+package com.example.rides.feature.presentation.ui.vehiclelist
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.rides.R
+
+class VehicleListFragment : Fragment(R.layout.fragment_vehicle_list) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
