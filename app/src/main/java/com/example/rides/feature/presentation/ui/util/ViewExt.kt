04b756cbe6544d61c43a5579cd4e74b469af5538
Redani Omar <101639366+RedaniOmar@users.noreply.github.com>
2023-01-11 20:54:00 +0100

    displaying input field and button

---
+++ app/src/main/java/com/example/rides/feature/presentation/ui/util/ViewExt.kt
@@ -0,0 +1,16 @@
+package com.example.rides.feature.presentation.ui.util
+
+import androidx.appcompat.widget.SearchView
+
+inline fun SearchView.onQueryTextSubmitted(crossinline listener: (String) -> Unit) {
+    this.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+        override fun onQueryTextSubmit(query: String?): Boolean {
+            listener(query.orEmpty())
+            return true
+        }
+
+        override fun onQueryTextChange(newText: String?): Boolean {
+            return true
+        }
+    })
+}
--- app/src/main/java/com/example/rides/feature/presentation/ui/vehiclelist/VehicleListFragment.kt
+++ app/src/main/java/com/example/rides/feature/presentation/ui/vehiclelist/VehicleListFragment.kt
@@ -1,17 +1,42 @@
 package com.example.rides.feature.presentation.ui.vehiclelist
 
 import android.os.Bundle
+import android.view.*
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
+import androidx.lifecycle.Lifecycle
 import com.example.rides.R
+import com.example.rides.feature.presentation.ui.util.onQueryTextSubmitted
 
 class VehicleListFragment : Fragment(R.layout.fragment_vehicle_list) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val menuHost: MenuHost = requireActivity()
+        menuOptions(menuHost)
+    }
 
+    private fun menuOptions(menuHost: MenuHost) {
+        menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.search_menu, menu)
+                val searchItem = menu.findItem(R.id.action_search)
+                val searchView = searchItem.actionView as SearchView
+                searchView.onQueryTextSubmitted {
+                    Toast.makeText(requireContext(), "click performed $it", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                return true
+            }
+
+
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
     }
 
 }
